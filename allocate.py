acb4b7a8bc5e9ffc22aa6421a88c4a9be66926a9
Mike Bryant <mike@mikebryant.me.uk>
2016-10-03 23:54:49 +0100

    Add ipv4 allocation

--- allocate.py
+++ allocate.py
@@ -1,9 +1,38 @@
 #!/usr/bin/env python3
 
+import etcd
 import netaddr
 import netifaces
 import sys
 import textwrap
+
+
+def get_etcd_client():
+    return etcd.Client(host='fd65:7b9c:569:680:98eb:c508:ea6b:b0b2')
+
+
+def get_config(key):
+    return get_etcd_client().read("/kubermesh.github.io/ip-allocator/config/%s" % key).value
+
+
+def allocate_address_etcd(machine_identity):
+    network = netaddr.IPNetwork(get_config('ipv4-base-network'))
+    client = get_etcd_client()
+    try:
+        response = client.read('/kubermesh.github.io/ip-allocator/ipv4/', recursive=True)
+    except etcd.EtcdKeyNotFound:
+        pass
+    else:
+        for child in response.children:
+            if child.value == machine_identity:
+                return child.key.replace('/kubermesh.github.io/ip-allocator/ipv4/', '')
+
+    for address in network:
+        try:
+            client.write("/kubermesh.github.io/ip-allocator/ipv4/%s" % address, machine_identity, prevExist=False)
+            return netaddr.IPAddress(address)
+        except etcd.EtcdAlreadyExist:
+            pass
 
 
 def mac_addresses():
@@ -38,20 +67,22 @@ def write_dummy_netdev_unit_file():
         fobj.write(dummy0_unit)
 
 
-def write_network_unit_file(interface_name, address, dhcp='both'):
+def write_network_unit_file(interface_name, ipv4_address, ipv6_address, dhcp='both'):
     unit = textwrap.dedent('''
         [Match]
         Name=%(interface_name)s
 
         [Address]
-        Address=%(address)s
+        Address=%(ipv4_address)s
+        Address=%(ipv6_address)s
 
         [Network]
         DHCP=%(dhcp)s
     ''' % {
-        'address': address,
         'dhcp': dhcp,
         'interface_name': interface_name,
+        'ipv4_address': ipv4_address,
+        'ipv6_address': ipv6_address,
     })
     with open("/target/units/%s.network" % interface_name, 'w') as fobj:
         fobj.write(unit)
@@ -79,11 +110,17 @@ def write_kubelet_opts_file(host_network):
 
 
 def main(argv):
-    _, base_address = argv
+    _, machine_identity = argv
 
-    host_network, pod_network = compute_networks(base_address)
+    host_network, pod_network = compute_networks(get_config('ipv6-base-network'))
+    ipv4_address = allocate_address_etcd(machine_identity)
+
+    assert host_network
+    assert pod_network
+    assert ipv4_address
+
     write_dummy_netdev_unit_file()
-    write_network_unit_file('dummy0', host_network)
+    write_network_unit_file('dummy0', ipv4_address, host_network)
     write_docker_opts_file(pod_network)
     write_kubelet_opts_file(host_network)
 
